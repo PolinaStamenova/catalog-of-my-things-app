97b991737c5a833563a05496cc9eeb807dc4b28a
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-19 00:29:11 +0300

    Update - use named parameters

--- lib/book.rb
+++ lib/book.rb
@@ -4,12 +4,20 @@ class Book < Item
 
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, *args )
-    super(*args)
-    @publisher = publisher
-    @cover_state = cover_state
+  def initialize(params = {})
+    super
+    @publisher = params[:publisher]
+    @cover_state = params[:cover_state]
+  end
+
+  private
+
+  def can_be_archieved?
+    return true if super || @cover_state == "bad"
+
+    false
   end
 end
 
-book = Book.new(publisher: "Mike", cover_state: "bad", publish_date: "2020-05-05")
-p book
+# book = Book.new(publisher: "Mike", cover_state: "bad", publish_date: "2020-05-05")
+# p book
