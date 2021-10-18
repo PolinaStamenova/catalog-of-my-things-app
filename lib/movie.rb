46566c80174921992f1f9099209342152a8e505c
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-19 00:45:17 +0300

    Add Movie class

---
+++ lib/movie.rb
@@ -0,0 +1,18 @@
+require_relative "item"
+
+class Movie < Item
+
+  attr_accessor :silet
+
+  def initialize(params = {})
+    super
+    @silet = params[:silet]
+  end
+
+  private
+
+  def can_be_archieved?
+    return true if super || @silet == true
+
+    false
+  end
